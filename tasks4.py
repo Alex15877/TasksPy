923c657a060b4c47f98ab9932bc48c389b5bb8ca
Alex15877 <51477800+Alex15877@users.noreply.github.com>
2019-10-13 15:40:30 +0300

    Add files via upload

--- tasks4.py
+++ tasks4.py
@@ -1,24 +1,13 @@
 while True:
     try:
-        oper = input("Выберите опреацию (+, -, *, /): ")
+        op = int(input("Выберите опреацию: 1) +, 2) -, 3) *, 4) /): "))
     except ValueError:
         print("Выберите операцию из указаных выше.")
         continue
+    d = [(1, chr(43)), (2, chr(45)), (3, chr(42)), (4, chr(47))]
     firstdigit = float(input("Введите первое число:"))
     seconddigit = float(input("Введите второе число:"))
-    if oper == "+":
-        result = firstdigit + seconddigit
-        print("Результат: " + str(result))
-    elif oper == "-":
-        result = firstdigit - seconddigit
-        print("Результат: " + str(result))
-    elif oper == "*":
-        result = firstdigit * seconddigit
-        print("Результат: " + str(result))
-    elif oper == "/":
-        result = firstdigit / seconddigit
-        print("Результат: " + str(result))
-    else:
-        print("Выбрана неверная операция!\n"
-              "Не целые числа вводятся через точку(.)")
+    g = op in d
+    res = firstdigit + g + seconddigit
+    print(res)
     break
