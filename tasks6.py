24c4608e2b0101c2a304a1c286f0c03fa6fd4f18
Alex15877 <51477800+Alex15877@users.noreply.github.com>
2019-11-01 23:11:59 +0200

    Add files via upload

---
+++ tasks6.py
@@ -0,0 +1,53 @@
+# Задание 1! Разобраться в работе декоратора functools.wraps
+from functools import wraps
+import logging
+
+
+def bib_1(func):
+    @wraps(func)
+    def wrapper():
+        print("Фантастика,\n Комедия,\n Детектив.")
+        return func()
+    return wrapper
+
+
+@bib_1
+def genres():
+    """Жанры"""
+
+
+print(genres.__name__)
+print(genres.__doc__)
+
+
+# Задание 2! Создать декоратор, который будет логировать поведение функции. Записывать действия и сохранять в файл.
+def log(func):
+
+    def wrap_log(*args, **kwargs):
+        name = func.__name__
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+
+        # Создаем файл логов для записи
+        fh = logging.FileHandler("list.log")
+        fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        formatter = logging.Formatter(fmt)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+        result = func(*args, **kwargs)
+        logger.info("Result %s" % result)
+        return func
+    return wrap_log
+
+
+@log
+def my_random(a):
+    """a + random digit"""
+    import random
+    r = random.randint(0, 100)
+    return a + r
+
+
+if __name__ == "__main__":
+    value = my_random(12) # a = 12
